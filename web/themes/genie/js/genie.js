3130221611ffdd0a858d38e0f6a54cbfb62858d7
bruriahelfer <bruriahelfer@gmail.com>
2025-11-27 15:13:35 +0200

    new HP

--- web/themes/genie/js/genie.js
+++ web/themes/genie/js/genie.js
@@ -15,16 +15,16 @@
               return elementBottom > viewportTop && elementTop < viewportBottom;
             };
             $(window).on('resize scroll', function() {
-                var delay = 0; // Reset delay here, inside the event handler
+              if ($('.paragraph--type--grid.grid_3.id-1 .field--name-field-items').isInViewport()) {
+                var delay = 0.2; // Reset delay here, inside the event handler
                 $('.paragraph--type--grid.grid_3.id-1 .field--name-field-items > .field__item').each(function(index) {                 
                   $(this).css('transition-delay', delay + 's');
                     delay = delay + 0.3;
+
                 });
-              $('.paragraph--type--grid.grid_3.id-1 .field--name-field-items > .field__item').each(function() {
-                if ($(this).isInViewport()) {
-                  $(this).addClass('inview');
-                }
-              })
+                
+                $('.paragraph--type--grid.grid_3.id-1').addClass('inview');
+              }
             });
 
 			$(".header .field--name-field-link a").on('click',function(e) {
