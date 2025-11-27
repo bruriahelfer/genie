8405b37358553969487d140e912f992db3b505a7
bruriahelfer <bruriahelfer@gmail.com>
2025-11-27 12:55:46 +0200

    new HP

--- web/themes/genie/js/genie.js
+++ web/themes/genie/js/genie.js
@@ -5,6 +5,24 @@
   Drupal.behaviors.my_custom_behavior = {
     attach: function (context, settings) {
 
+            $.fn.isInViewport = function() {
+              var elementTop = $(this).offset().top;
+              var elementBottom = elementTop + $(this).outerHeight();
+
+              var viewportTop = $(window).scrollTop();
+              var viewportBottom = viewportTop + $(window).height();
+
+              return elementBottom > viewportTop && elementTop < viewportBottom;
+            };
+
+            $(window).on('resize scroll', function() {
+              if ($('.paragraph--type--grid').isInViewport()) {
+                $('.paragraph--type--grid').addClass('inview');
+              } else {
+                $('.paragraph--type--grid').removeClass('inview');
+              }
+            });
+
 			$(".header .field--name-field-link a").on('click',function(e) {
 				var url = e.target.href;
 				var hash = url.substring(url.indexOf("#")+1);
